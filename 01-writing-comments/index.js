45ca1d43e93d1f20285c1b3b6dbcecc5c485a23a
mrikihidayat <m.rikihidayat2@gmail.com>
2024-09-26 07:57:41 +0700

    commit kesatu

---
+++ 01-writing-comments/index.js
@@ -0,0 +1,9 @@
+// 1. Buka berkas `index.js` dan tuliskan "username" akun Dicoding Anda dalam bentuk **komentar satu baris**.
+// rikihidayat27
+
+
+// 2. Buka berkas `index.js` dan tuliskan teks di bawah ini dalam bentuk **komentar banyak baris**.
+/*text
+Goal tahun ini:
+1. Belajar JavaScript.
+2. Menjadi Front-End atau Back-End Developer.*/
